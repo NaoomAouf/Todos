5e14095695c1795e37739c68d0112cc19d1c1500
NaoomAouf <s665183@ese.gov.ae>
2023-07-30 16:30:57 +0400

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Naoom Aouf on 30/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
